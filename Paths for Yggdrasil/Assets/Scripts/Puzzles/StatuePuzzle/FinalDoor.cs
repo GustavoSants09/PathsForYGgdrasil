e0d79c138d6f859668e1b335f14c8cbd98594280
GustavoSants09 <143751175+GustavoSants09@users.noreply.github.com>
2025-07-08 10:33:54 -0300

    prog pronta para a alpha

--- Paths for Yggdrasil/Assets/Scripts/Puzzles/StatuePuzzle/FinalDoor.cs
+++ Paths for Yggdrasil/Assets/Scripts/Puzzles/StatuePuzzle/FinalDoor.cs
@@ -5,12 +5,26 @@ public class FinalDoor : MonoBehaviour, Interectable
     public string message = "Você concluiu o puzzle! Parabéns!";
     public bool canInteract = false; // Controla se a porta pode ser usada
 
+    public GameObject panelToShow; // 👈 Painel a ser ativado na interação
+    public BoxCollider player;
+    
+
+
     public void Interact()
     {
         if (!canInteract) return;
 
         Debug.Log(message);
-        // Aqui você pode adicionar lógica para mostrar painel de UI ou finalizar a alpha
+
+        if (panelToShow != null)
+        {
+            panelToShow.SetActive(true);
+            Debug.Log("🔓 Painel de porta ativado.");
+            player.enabled = false;
+            this.enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     public string GetDescription()
