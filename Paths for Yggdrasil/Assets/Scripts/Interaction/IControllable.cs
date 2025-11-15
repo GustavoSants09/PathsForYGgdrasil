64a2211f84f10b748b4138b4d2e793767ff2d480
GustavoSants09 <143751175+GustavoSants09@users.noreply.github.com>
2025-11-15 18:37:34 -0300

    limpando o projeto

--- Paths for Yggdrasil/Assets/Scripts/Interaction/IControllable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IControllable
-{
-    void RequestPickUp(Transform holdPoint = null);
-    void Drop();
-}
--- Paths for Yggdrasil/Assets/Scripts/Interaction/Interectable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface Interectable
-{
-    void Interact();
-
-    string GetDescription();
-}
